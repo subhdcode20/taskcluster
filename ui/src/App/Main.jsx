b18b5eed67666874403aaabf50a9d644cfe748df
Subhdcode <subham@neargroup.me>
2019-06-06 13:41:36 +0530

    @hotloader/react-dom and material makeStyles hook experiments

--- ui/src/App/Main.jsx
+++ ui/src/App/Main.jsx
@@ -1,47 +1,48 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { object, arrayOf } from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import RouteWithProps from '../components/RouteWithProps';
 import ErrorPanel from '../components/ErrorPanel';
 import { route } from '../utils/prop-types';
 
-const styles = theme => ({
-  '@global': {
-    [[
-      'input:-webkit-autofill',
-      'input:-webkit-autofill:hover',
-      'input:-webkit-autofill:focus',
-      'input:-webkit-autofill:active',
-    ].join(',')]: {
-      transition:
-        'background-color 5000s ease-in-out 0s, color 5000s ease-in-out 0s',
-      transitionDelay: 'background-color 5000s, color 5000s',
+const useStyles = makeStyles(theme => ({
+    '@global': {
+      [[
+        'input:-webkit-autofill',
+        'input:-webkit-autofill:hover',
+        'input:-webkit-autofill:focus',
+        'input:-webkit-autofill:active',
+      ].join(',')]: {
+        transition:
+          'background-color 5000s ease-in-out 0s, color 5000s ease-in-out 0s',
+        transitionDelay: 'background-color 5000s, color 5000s',
+      },
+      '.mdi-icon': {
+        fill: theme.palette.text.primary,
+      },
+      '.CodeMirror': {
+        fontSize: 13,
+        height: '100% !important',
+      },
+      '[disabled] .mdi-icon': {
+        fill: theme.palette.primary.light,
+      },
+      a: {
+        color: theme.palette.text.primary,
+      },
+      'html, body': {
+        color: theme.palette.text.secondary,
+      },
+      pre: {
+        overflowX: 'auto',
+      },
+      ':not(pre) > code': {
+        ...theme.mixins.highlight,
+      },
     },
-    '.mdi-icon': {
-      fill: theme.palette.text.primary,
-    },
-    '.CodeMirror': {
-      fontSize: 13,
-      height: '100% !important',
-    },
-    '[disabled] .mdi-icon': {
-      fill: theme.palette.primary.light,
-    },
-    a: {
-      color: theme.palette.text.primary,
-    },
-    'html, body': {
-      color: theme.palette.text.secondary,
-    },
-    pre: {
-      overflowX: 'auto',
-    },
-    ':not(pre) > code': {
-      ...theme.mixins.highlight,
-    },
-  },
-});
+  })
+);
 const MainHooks = ({ error, routes }) => (
   <Fragment>
     <ErrorPanel error={error} />
@@ -64,4 +65,4 @@ MainHooks.defaultProps = {
   error: null,
 };
 
-export default withStyles(styles)(MainHooks);
+export default MainHooks;
